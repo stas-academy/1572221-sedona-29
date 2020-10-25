a93fc63c1c37709680d48448747dff5675092148
Stas <stanislav100z@mail.ru>
2020-10-25 13:39:27 +0300

    Правки 2

--- js/script.js
+++ js/script.js
@@ -1,9 +1,10 @@
 const reservationButton = document.querySelector(".reservation-button");
 const reservationPopup = document.querySelector(".search-form");
-const checkIn = reservationPopup.querySelector("[name=check-in]")
+const checkIn = reservationPopup.querySelector("[name=check-in]");
 
 reservationButton.addEventListener("click", function (evt) {
   evt.preventDefault();
   reservationPopup.classList.toggle("search-form-hide");
   checkIn.focus();
 });
+
