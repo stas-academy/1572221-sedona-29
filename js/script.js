de34864e24fc7b40af17f4bd560f364ec64d1579
Stas <stanislav100z@mail.ru>
2020-10-26 20:57:05 +0300

    Js анимация, и прочие попраки

--- js/script.js
+++ js/script.js
@@ -1,9 +1,24 @@
 const reservationButton = document.querySelector(".reservation-button");
 const reservationPopup = document.querySelector(".search-form");
 const checkIn = reservationPopup.querySelector("[name=check-in]");
+const form = document.querySelector(".search-form-error");
+const dateIn = document.querySelector(".date-in");
+const dateOut = document.querySelector(".date-out");
 
 reservationButton.addEventListener("click", function (evt) {
   evt.preventDefault();
   reservationPopup.classList.toggle("search-form-hide");
+  reservationPopup.classList.remove("modal-error");
   checkIn.focus();
 });
+form.addEventListener("submit", function (evt) {
+  if (!dateIn.value || !dateOut.value) {
+    evt.preventDefault();
+    reservationPopup.classList.add("modal-error");
+  }
+});
+
+
+
+
+
